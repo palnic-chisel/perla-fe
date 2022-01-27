4c83788d0215fb2e2e4cd82621035761e59d1bcf
palnic-chisel <77287924+nicola-palumbo@users.noreply.github.com>
2022-01-27 19:44:36 +0100

    width frame

--- src/component/FrameWithBackgroundComponent/FrameWithBackgroundComponent.jsx
+++ src/component/FrameWithBackgroundComponent/FrameWithBackgroundComponent.jsx
@@ -14,63 +14,58 @@ function FrameWithBackgroundComponent({ data }) {
   }, []);
 
   return (
-    <div className="frameB-container">
-      <div
-        className="frameB-inner-container"
-        style={{
-          // alignItems: data.captionPosition === 'Left' ? "flex-start" : "flex-end",
-          // minHeight: data.frameSize === "Big" ? "100vh" : "50vh",
-        }}
-      >
 
-        <div className="frameB-text-container" style={{ textAlign: data.captionPosition }}>
-          <div className="frameB-title-container">
-            <span data-aos="fade-left" style={{ textAlign: data.captionPosition }}>
-              {data.title}
-            </span>
-            <h1
-              data-aos="fade-left"
-              className={data.captionPosition === 'right' ? 'frameB-title-right' : 'frameB-title-left'}
-              style={{ textAlign: data.captionPosition }}
-            >
-              {data.frameTitle}
-            </h1>
-          </div>
-          <div data-aos="fade-left" className="frameB-description" style={{ textAlign: data.captionPosition }}>
-            {data.frameSecondaryImageYesOrNot && <SecondaryImage position={data.captionPosition} imageUrl={`${data?.frameSecondaryImage?.url}`} />}
-            <div className="frameB-caption-container" style={{ paddingRight: data.captionPosition === 'Left' ? '50px' : '10px', paddingLeft: data.captionPosition === 'Right' ? '50px' : '10px' }}>
-              <div className="frame-caption"><ReactMarkdown>{data?.richCaption}</ReactMarkdown></div>
-            </div>
-          </div>
+    <div
+      className="frameB-inner-container"
+    >
 
+      <div className="frameB-text-container" style={{ textAlign: data.captionPosition }}>
+        <div className="frameB-title-container">
+          <span data-aos="fade-left" style={{ textAlign: data.captionPosition }}>
+            {data.title}
+          </span>
+          <h1
+            data-aos="fade-left"
+            className={data.captionPosition === 'right' ? 'frameB-title-right' : 'frameB-title-left'}
+            style={{ textAlign: data.captionPosition }}
+          >
+            {data.frameTitle}
+          </h1>
         </div>
-        {data.activeSeeMoreButton && (
-          <ButtonComponent
-            buttonText={data.seeMoreButtonText}
-            onClickFnc={() => {
-              // history.push(`/${data.frameButtonGoesTo}`)
-            }}
-          />
-        )}
-
-        {data?.backgroundImage?.url && (
-          <div style={{
-            content: '',
-            // backgroundImage: `url(${data.backgroundImage.url})`,
-            background: `url(${data?.backgroundImage?.url}) no-repeat center center fixed`,
-            backgroundSize: 'cover',
-            opacity: '0.5',
-            top: '0',
-            left: '0',
-            bottom: '0',
-            right: '0',
-            position: 'absolute',
-            zIndex: '-1',
-          }}
-          />
-        )}
-
+        <div data-aos="fade-left" className="frameB-description">
+          {data.frameSecondaryImageYesOrNot && <SecondaryImage position={data.captionPosition} imageUrl={`${data?.frameSecondaryImage?.url}`} />}
+          <div className="frameB-caption-container" style={{ paddingRight: data.captionPosition === 'Left' ? '50px' : '10px', paddingLeft: data.captionPosition === 'Right' ? '50px' : '10px' }}>
+            <div className="frame-caption"><ReactMarkdown>{data?.richCaption}</ReactMarkdown></div>
+          </div>
+        </div>
       </div>
+
+      {data.activeSeeMoreButton && (
+      <ButtonComponent
+        buttonText={data.seeMoreButtonText}
+        onClickFnc={() => {
+          // history.push(`/${data.frameButtonGoesTo}`)
+        }}
+      />
+      )}
+
+      {data?.backgroundImage?.url && (
+      <div style={{
+        content: '',
+        // backgroundImage: `url(${data.backgroundImage.url})`,
+        background: `url(${data?.backgroundImage?.url}) no-repeat center center fixed`,
+        backgroundSize: 'cover',
+        opacity: '0.5',
+        top: '0',
+        left: '0',
+        bottom: '0',
+        right: '0',
+        position: 'absolute',
+        zIndex: '-1',
+      }}
+      />
+      )}
+
     </div>
 
   );
