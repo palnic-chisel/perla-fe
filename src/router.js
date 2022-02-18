e20a4206a493578897470063f00da805be77389a
nicola.palumbo <nicola.palumbo@certimetergroup.com>
2022-02-18 12:48:03 +0100

    added sitemap

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+    <Switch>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/psicologia" />
+        <Route path="/mindfulness" />
+        <Route path="/contatti" />
+    </Switch>
+)
